8594ddab71598c0955642d8007399cef79098c19
Abdul Munhim Hussain <abdulmunhim2002@gmail.com>
2026-05-09 23:26:53 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-# my name is munhim
+def divide_numbers(a, b):
+    result = a / b
+    return result
+
+numbers = [1, 2, 3]
+
+print("Fourth element:", numbers[3])
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+print("Division result:", divide_numbers(x, y))
+
+name = "Ali"
+print("Age: " + 20)
